a4de920178cd55d6f3adde0a2b23f5fadef2f6b4
Vladimir Shidlovsky <vl.shidlovsky@gmail.com>
2017-05-30 11:25:08 +0300

    Improve histogram calculation.

--- hash.rb
+++ hash.rb
@@ -1,35 +1,3 @@
-def get_array
-  n = -1
-  while n < 1
-      print "Enter massive length: "
-      n = gets.to_i
-  end
-  array = Array.new(n)
-  i = 0
-  while i < n
-      print "Enter #{i}-th massive element: "
-      array[i] = gets.chomp
-      i = i+1
-  end
-
-  print("Your array:", array)
-  puts
-  array
+def histogram(array)
+  array.each_with_object(Hash.new(0)) { |el, hash| hash[el] += 1 }
 end
-
-filled_array = get_array()
-
-def repeats(filled_array)
-  find_repeats = Hash.new
-
-  filled_array.each do |element|
-    find_repeats[element] ? find_repeats[element] += 1 : find_repeats[element] = 1
-  end
-
-  print(find_repeats)
-  puts
-  find_repeats
-end
-
-repeats(filled_array)
-
